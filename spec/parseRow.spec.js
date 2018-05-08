74ea28d98a47b6eb57571a16166b84cf9d153b10
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-08 15:47:21 +0200

    Add simple form string extraction

--- spec/parseRow.spec.js
+++ spec/parseRow.spec.js
@@ -2,18 +2,21 @@ describe('parseRow', () => {
   const parseRow = require('../lib/parseRow')
 
   const examples = [
-    {name: 'lemma', row: '**lemma** description'},
-    {name: 'italic lemma', row: '***lemma*** description'},
-    {name: 'lemma with asterisk', row: '**\\*lemma** description', lemma: '*lemma'},
-    {name: 'italic lemma with asterisk', row: '**\\*lemma** description', lemma: '*lemma'},
-    {name: 'homonym', row: '**lemma** II description', homonym: 'II'}
+    {name: 'lemma', row: '**lemma** "definition"'},
+    {name: 'italic lemma', row: '***lemma*** "definition"'},
+    {name: 'lemma with asterisk', row: '**\\*lemma** "definition"', lemma: '*lemma'},
+    {name: 'italic lemma with asterisk', row: '**\\*lemma** "definition"', lemma: '*lemma'},
+    {name: 'homonym', row: '**lemma** II "definition"', homonym: 'II'},
+    {name: 'forms', row: '**lemma**, *form1*, *form2* "definition"', forms: ', *form1*, *form2*'},
+    {name: 'homonym + forms', row: '**lemma** IV, *form1* "definition"', forms: ', *form1*', homonym: 'IV'}
   ]
 
-  examples.forEach(({name, row, lemma = 'lemma', definition = 'description', homonym = 'I'}) => {
+  examples.forEach(({name, row, lemma = 'lemma', homonym = 'I', forms = '', definition = '"definition"'}) => {
     it(`extracts the data correctly from ${name}`, () => {
       expect(parseRow(row)).toEqual({
         lemma: lemma,
         homonym: homonym,
+        forms: forms,
         definition: definition,
         source: row
       })
