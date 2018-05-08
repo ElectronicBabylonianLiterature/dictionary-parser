11248be1e28db91da6a8e6e027a1f9eeb7340815
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-08 16:18:06 +0200

    Allow definitions in brackets

--- spec/parseRow.spec.js
+++ spec/parseRow.spec.js
@@ -1,24 +1,32 @@
 describe('parseRow', () => {
   const parseRow = require('../lib/parseRow')
 
-  const examples = [
-    {name: 'lemma', row: '**lemma** "definition"'},
-    {name: 'italic lemma', row: '***lemma*** "definition"'},
-    {name: 'lemma with asterisk', row: '**\\*lemma** "definition"', lemma: '*lemma'},
-    {name: 'italic lemma with asterisk', row: '**\\*lemma** "definition"', lemma: '*lemma'},
-    {name: 'homonym', row: '**lemma** II "definition"', homonym: 'II'},
-    {name: 'forms', row: '**lemma**, *form1*, *form2* "definition"', forms: ', *form1*, *form2*'},
-    {name: 'homonym + forms', row: '**lemma** IV, *form1* "definition"', forms: ', *form1*', homonym: 'IV'}
+  const definitions = [
+    '"definition"',
+    '(definition)'
+    // '**\\~** definition'
   ]
 
-  examples.forEach(({name, row, lemma = 'lemma', homonym = 'I', forms = '', definition = '"definition"'}) => {
-    it(`extracts the data correctly from ${name}`, () => {
-      expect(parseRow(row)).toEqual({
-        lemma: lemma,
-        homonym: homonym,
-        forms: forms,
-        definition: definition,
-        source: row
+  definitions.forEach(definition => {
+    const examples = [
+      {name: 'lemma', row: `**lemma** ${definition}`},
+      {name: 'italic lemma', row: `***lemma*** ${definition}`},
+      {name: 'lemma with asterisk', row: `**\\*lemma** ${definition}`, lemma: '*lemma'},
+      {name: 'italic lemma with asterisk', row: `**\\*lemma** ${definition}`, lemma: '*lemma'},
+      {name: 'homonym', row: `**lemma** II ${definition}`, homonym: 'II'},
+      {name: 'forms', row: `**lemma**, *form1*, *form2* ${definition}`, forms: ', *form1*, *form2*'},
+      {name: 'homonym + forms', row: `**lemma** IV, *form1* ${definition}`, forms: ', *form1*', homonym: 'IV'}
+    ]
+
+    examples.forEach(({name, row, lemma = 'lemma', homonym = 'I', forms = ''}) => {
+      it(`extracts the data correctly from ${row}`, () => {
+        expect(parseRow(row)).toEqual({
+          lemma: lemma,
+          homonym: homonym,
+          forms: forms,
+          definition: definition,
+          source: row
+        })
       })
     })
   })
