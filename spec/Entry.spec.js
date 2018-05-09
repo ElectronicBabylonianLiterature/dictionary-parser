f05bed0060fe4b66f4ace40b51699c6d70bffa2b
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-09 10:00:31 +0200

    Refactor Entry spec

--- spec/Entry.spec.js
+++ spec/Entry.spec.js
@@ -1,5 +1,30 @@
+function * cartesian (head, ...tail) {
+  // From https://stackoverflow.com/a/44344803
+  let remainder = tail.length ? cartesian(...tail) : [[]]
+  for (let r of remainder) for (let h of head) yield [h, ...r]
+}
+
 describe('Entry', () => {
   const Entry = require('../lib/Entry')
+
+  const lemmaExamples = [
+    ['**lemma**', 'lemma'],
+    ['***lemma***', 'lemma'],
+    ['**\\*lemma**', '*lemma'],
+    ['***\\*lemma***', '*lemma']
+  ]
+
+  const homonymExamples = [
+    ['', 'I'],
+    [' II', 'II'],
+    [' VII', 'VII']
+  ]
+
+  const formExamples = [
+    '',
+    ', *form1*, *form2*',
+    ', *form1*'
+  ]
 
   const definitions = [
     '"definition"',
@@ -7,37 +32,27 @@ describe('Entry', () => {
     '**\\~** definition'
   ]
 
-  definitions.forEach(definition => {
-    const examples = [
-      {row: `**lemma** ${definition}`},
-      {row: `***lemma*** ${definition}`},
-      {row: `**\\*lemma** ${definition}`, lemma: '*lemma'},
-      {row: `**\\*lemma** ${definition}`, lemma: '*lemma'},
-      {row: `**lemma** II ${definition}`, homonym: 'II'},
-      {row: `**lemma**, *form1*, *form2* ${definition}`, forms: ', *form1*, *form2*'},
-      {row: `**lemma** IV, *form1* ${definition}`, forms: ', *form1*', homonym: 'IV'}
-    ]
+  for (const [lemma, homonym, forms, definition] of cartesian(lemmaExamples, homonymExamples, formExamples, definitions)) {
+    const row = `${lemma[0]}${homonym[0]}${forms} ${definition}`
 
-    examples.forEach(({row, lemma = 'lemma', homonym = 'I', forms = ''}) => {
-      describe(`row is: ${row}`, () => {
-        const entry = new Entry(row)
+    describe(`row is: ${row}`, () => {
+      const entry = new Entry(row)
 
-        it('parses lemma correctly', () => {
-          expect(entry.lemma).toEqual(lemma)
-        })
-        it('parses homonym correctly', () => {
-          expect(entry.homonym).toEqual(homonym)
-        })
-        it('parses forms correctly', () => {
-          expect(entry.forms).toEqual(forms)
-        })
-        it('parses definition correctly', () => {
-          expect(entry.definition).toEqual(definition)
-        })
-        it('sets source to original row', () => {
-          expect(entry.source).toEqual(row)
-        })
+      it('parses lemma correctly', () => {
+        expect(entry.lemma).toEqual(lemma[1])
+      })
+      it('parses homonym correctly', () => {
+        expect(entry.homonym).toEqual(homonym[1])
+      })
+      it('parses forms correctly', () => {
+        expect(entry.forms).toEqual(forms)
+      })
+      it('parses definition correctly', () => {
+        expect(entry.definition).toEqual(definition)
+      })
+      it('sets source to original row', () => {
+        expect(entry.source).toEqual(row)
       })
     })
-  })
+  }
 })
