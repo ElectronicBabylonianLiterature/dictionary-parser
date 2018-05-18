8470e52cbf3f096da5258566dd02c8585e341f2f
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-18 12:42:52 +0200

    Use named groups in parseNotes

--- spec/parseNotes.spec.js
+++ spec/parseNotes.spec.js
@@ -2,18 +2,18 @@ describe('parseNotes', () => {
   const parseNotes = require('../lib/parseNotes')
 
   it('returns specified indices', () => {
-    expect(parseNotes([0, 2], ['a', 'b', 'c'])).toEqual(['a', 'c'])
+    expect(parseNotes(['a', 'c'], {a: 'a', b: 'b', c: 'c'})).toEqual(['a', 'c'])
   })
 
   it('trims notes', () => {
-    expect(parseNotes([0], [' a '])).toEqual(['a'])
+    expect(parseNotes(['a'], {a: ' a '})).toEqual(['a'])
   })
 
   it('filters out empty strings', () => {
-    expect(parseNotes([0], [' '])).toEqual([])
+    expect(parseNotes(['a'], {a: ' '})).toEqual([])
   })
 
   it('filters out nil values', () => {
-    expect(parseNotes([0, 1], [undefined, null])).toEqual([])
+    expect(parseNotes(['a', 'b', 'c'], {a: undefined, b: null})).toEqual([])
   })
 })
