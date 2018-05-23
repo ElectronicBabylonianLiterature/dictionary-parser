aa7abb6cc91e5f3b8c3681c3ff5d97e86ba5b80e
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-23 11:59:50 +0200

    Introduce Link

---
+++ spec/Link.spec.js
@@ -0,0 +1,32 @@
+describe('Link', () => {
+  const Link = require('../lib/Link')
+
+  describe('isLink', () => {
+    [
+      '**\\*source** *cf.* *target*',
+      '**\\*source** *cf.* *target* note',
+      '**\\*source** *cf.* *target',
+      '**source** *cf.* *target* extra',
+      '**source** etc. *cf.* *target*',
+      '**source**, **source2** *cf.* *target*',
+      '**source1** **source2** *cf.* *target*'
+    ].forEach(row => {
+      it(`${row} is a link`, () => {
+        expect(Link.isLink(row)).toEqual(true)
+      })
+    });
+
+    [
+      '**lemma** meaning; *cf.* *derived*',
+      '**lemma** \\[LOG\\]; *cf.* *derived*'
+    ].forEach(row => {
+      it(`${row} is not a link`, () => {
+        expect(Link.isLink(row)).toEqual(false)
+      })
+    })
+  })
+
+  it('has source', () => {
+    expect(new Link('source').source).toEqual('source')
+  })
+})
