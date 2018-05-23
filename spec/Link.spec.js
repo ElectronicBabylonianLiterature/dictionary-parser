97d80899fd61dbe3f8e3e61c2bddaf23aac4d748
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-23 16:02:57 +0200

    Parse multiple lemmas in links

--- spec/Link.spec.js
+++ spec/Link.spec.js
@@ -9,6 +9,7 @@ describe('Link', () => {
       '**\\*source** *cf.* *target* note',
       '**\\*source** *cf.* *target',
       '**source** *cf.* *target* extra',
+      '**-source** *cf.* *-target*',
       '**source** etc. *cf.* *target*',
       '**source**, **source2** *cf.* *target*',
       '**source1 source2** *cf.* *target* 5'
@@ -31,6 +32,12 @@ describe('Link', () => {
   it('parses lemma correctly', () => {
     const lemma = 'lemma(1) \\*lemma\\[2]\\'
     expect(new Link(`**${lemma}** *cf.* *target*`).lemmas).toEqual(parseLemma(lemma))
+  })
+
+  it('parses multiple lemmas correctly', () => {
+    const lemma1 = 'lemma(1) \\*lemma\\[2]\\'
+    const lemma2 = 'lemma(a) \\*lemma\\[b]\\'
+    expect(new Link(`**${lemma1}**, **${lemma2}** *cf.* *target*`).lemmas).toEqual(parseLemma(lemma1).concat(parseLemma(lemma2)))
   })
 
   it('has source', () => {
