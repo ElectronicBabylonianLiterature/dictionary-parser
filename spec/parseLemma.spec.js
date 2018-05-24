10cb5d487c08d009d51da1381b6b21c8e7014223
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-24 14:25:06 +0200

    Expand -um at the end of lemma

--- spec/parseLemma.spec.js
+++ spec/parseLemma.spec.js
@@ -5,8 +5,12 @@ describe('parseLemma', () => {
     expect(parseLemma('\\*lem\\[ma\\]')).toEqual([['*lem[ma]']])
   })
 
-  it('expands parentheses and sort by length', () => {
+  it('expands parentheses', () => {
     expect(parseLemma('lemm(a)')).toEqual([['lemm'], ['lemma']])
+  })
+
+  it('expands -um', () => {
+    expect(parseLemma('lemmum')).toEqual([['lemmu'], ['lemmum']])
   })
 
   it('parses compound lemmas', () => {
