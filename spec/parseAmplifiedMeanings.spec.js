b4201cad39361d0c21c8022fd92d4a43f2e7f344
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-25 09:44:32 +0200

    Extract simple vowels from conjugations and functions

--- spec/parseAmplifiedMeanings.spec.js
+++ spec/parseAmplifiedMeanings.spec.js
@@ -6,7 +6,12 @@ const examples = [
   [' **A.** meaning **B.** meaning;', {'A.': {meaning: 'meaning'}, 'B.': {meaning: 'meaning'}}],
   [' **A.** "meaning1; meaning2";', {'A.': {meaning: '"meaning1; meaning2"'}}],
   [' **A.** **1.** "meaning1; meaning2";', {'A.': {'1.': '"meaning1; meaning2"'}}],
-  [' **1.** MB, NB (meaning) **2.** meaning', {implicit: {'1.': 'MB, NB (meaning)', '2.': 'meaning'}}]
+  [' **1.** MB, NB (meaning) **2.** meaning', {implicit: {'1.': 'MB, NB (meaning)', '2.': 'meaning'}}],
+  [' **G** meaning (*u/a*)', {'G': {meaning: 'meaning (*u/a*)', vowels: ['u', 'a']}}]
+  // [' **G** meaning (*a/u*, occas. *u/u*, *i/i*)', {'G': {meaning: 'meaning', vowels: ['u', 'a']}}],
+  // [' **G** meaning (*a/u*; notes *lemma*)', {'G': {meaning: 'meaning', vowels: ['u', 'a']}}],
+  // [' **G** meaning (*a/u*, notes *lemma*)', {'G': {meaning: 'meaning', vowels: ['u', 'a']}}]
+  // [' **G** meaning **1.** meaning (*u/a*)', {'G': {meaning: 'meaning', vowels: ['u', 'a']}}]
 ]
 
 describe('parseAmplifiedMeanings', () => {
