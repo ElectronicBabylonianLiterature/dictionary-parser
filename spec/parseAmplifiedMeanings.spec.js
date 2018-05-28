1c3038cbd1086e0ef9e95369b0c60976845cb386
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-28 13:30:57 +0200

    Extract vowels with note

--- spec/parseAmplifiedMeanings.spec.js
+++ spec/parseAmplifiedMeanings.spec.js
@@ -7,8 +7,34 @@ const examples = [
   [' **A.** "meaning1; meaning2";', {'A.': {meaning: '"meaning1; meaning2"'}}],
   [' **A.** **1.** "meaning1; meaning2";', {'A.': {'1.': '"meaning1; meaning2"'}}],
   [' **1.** MB, NB (meaning) **2.** meaning', {implicit: {'1.': 'MB, NB (meaning)', '2.': 'meaning'}}],
-  [' **G** meaning (*u/a*)', {'G': {meaning: 'meaning (*u/a*)', vowels: [['u', 'a']]}}],
-  [' **G** meaning (*a/u*, *u/u*, *i/i*)', {'G': {meaning: 'meaning (*a/u*, *u/u*, *i/i*)', vowels: [['a', 'u'], ['u', 'u'], ['i', 'i']]}}]
+  [' **G** meaning (*a/u*)', {'G': {meaning: 'meaning (*a/u*)', vowels: [{value: ['a', 'u'], notes: []}]}}],
+  [' **G** meaning (*a/u*, *u/u*, *i/i*)',
+    {'G': {
+      meaning: 'meaning (*a/u*, *u/u*, *i/i*)',
+      vowels: [
+        {value: ['a', 'u'], notes: []},
+        {value: ['u', 'u'], notes: []},
+        {value: ['i', 'i'], notes: []}
+      ]
+    }}
+  ],
+  [' **G** meaning (*a/u*, occas. *u/u*)',
+    {'G': {
+      meaning: 'meaning (*a/u*, occas. *u/u*)',
+      vowels: [
+        {value: ['a', 'u'], notes: []},
+        {value: ['u', 'u'], notes: ['occas.']}
+      ]
+    }}
+  ],
+  [' **G** meaning (OB mostly *lemma1*, *lemma2*, *lemma3*, later Bab. *u/u*)',
+    {'G': {
+      meaning: 'meaning (OB mostly *lemma1*, *lemma2*, *lemma3*, later Bab. *u/u*)',
+      vowels: [
+        {value: ['u', 'u'], notes: ['OB mostly *lemma1*, *lemma2*, *lemma3*, later Bab.']}
+      ]
+    }}
+  ]
 ]
 
 describe('parseAmplifiedMeanings', () => {
