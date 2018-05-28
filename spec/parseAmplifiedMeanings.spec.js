b9a27b7595b0639a12c3e860e12884926217e843
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-05-28 15:14:23 +0200

    Extract vowels from entries

--- spec/parseAmplifiedMeanings.spec.js
+++ spec/parseAmplifiedMeanings.spec.js
@@ -2,14 +2,15 @@ const extractVowels = require('../lib/extractVowels')
 
 const examples = [
   ['', {}],
-  [' **1.** meaning **2.** meaning;', {implicit: {'1.': 'meaning', '2.': 'meaning'}}],
-  [' **G** meaning **1.** meaning **2.** meaning;', {'G': {meaning: 'meaning', '1.': 'meaning', '2.': 'meaning'}}],
-  [' **D** **1.** meaning;', {'D': {'1.': 'meaning'}}],
+  [' **1.** meaning **2.** meaning;', {implicit: {'1.': {meaning: 'meaning'}, '2.': {meaning: 'meaning'}}}],
+  [' **G** meaning **1.** meaning **2.** meaning;', {'G': {meaning: 'meaning', '1.': {meaning: 'meaning'}, '2.': {meaning: 'meaning'}}}],
+  [' **D** **1.** meaning;', {'D': {'1.': {meaning: 'meaning'}}}],
   [' **A.** meaning **B.** meaning;', {'A.': {meaning: 'meaning'}, 'B.': {meaning: 'meaning'}}],
   [' **A.** "meaning1; meaning2";', {'A.': {meaning: '"meaning1; meaning2"'}}],
-  [' **A.** **1.** "meaning1; meaning2";', {'A.': {'1.': '"meaning1; meaning2"'}}],
-  [' **1.** MB, NB (meaning) **2.** meaning', {implicit: {'1.': 'MB, NB (meaning)', '2.': 'meaning'}}],
-  [' **G** meaning (*a/u*)', {'G': {meaning: 'meaning (*a/u*)', vowels: extractVowels('meaning (*a/u*)')}}]
+  [' **A.** **1.** "meaning1; meaning2";', {'A.': {'1.': {meaning: '"meaning1; meaning2"'}}}],
+  [' **1.** MB, NB (meaning) **2.** meaning', {implicit: {'1.': {meaning: 'MB, NB (meaning)'}, '2.': {meaning: 'meaning'}}}],
+  [' **G** meaning (*a/u*)', {'G': {meaning: 'meaning (*a/u*)', vowels: extractVowels('meaning (*a/u*)')}}],
+  [' **G** **1.** (*a/u*) "meaning";', {'G': {'1.': {meaning: '(*a/u*) "meaning"', vowels: extractVowels('(*a/u*)')}}}]
 ]
 
 describe('parseAmplifiedMeanings', () => {
