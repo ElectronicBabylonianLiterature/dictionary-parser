6ee1b6f027c998a5c2ce00ab18b5bd832728e927
Jussi Laasonen <jussi.laasonen@lmu.de>
2018-06-18 15:57:37 +0200

    Always add meaning and vowels to amplified meanings

--- spec/parseAmplifiedMeanings.spec.js
+++ spec/parseAmplifiedMeanings.spec.js
@@ -2,15 +2,34 @@ const extractVowels = require('../lib/extractVowels')
 
 const examples = [
   ['', {}],
-  [' **1.** meaning **10.** meaning;', {implicit: {'1.': {meaning: 'meaning'}, '10.': {meaning: 'meaning'}}}],
-  [' **G** meaning **1.** meaning **2.** meaning;', {'G': {meaning: 'meaning', '1.': {meaning: 'meaning'}, '2.': {meaning: 'meaning'}}}],
-  [' **D** **1.** meaning;', {'D': {'1.': {meaning: 'meaning'}}}],
-  [' **A.** meaning **B.** meaning;', {'A.': {meaning: 'meaning'}, 'B.': {meaning: 'meaning'}}],
-  [' **A.** "meaning1; meaning2";', {'A.': {meaning: '"meaning1; meaning2"'}}],
-  [' **A.** **1.** "meaning1; meaning2";', {'A.': {'1.': {meaning: '"meaning1; meaning2"'}}}],
-  [' **1.** MB, NB (meaning) **2.** meaning', {implicit: {'1.': {meaning: 'MB, NB (meaning)'}, '2.': {meaning: 'meaning'}}}],
-  [' **G** meaning (*a/u*)', {'G': {meaning: 'meaning (*a/u*)', vowels: extractVowels('meaning (*a/u*)')}}],
-  [' **G** **1.** (*a/u*) "meaning";', {'G': {'1.': {meaning: '(*a/u*) "meaning"', vowels: extractVowels('(*a/u*)')}}}]
+  [' **1.** meaning **10.** meaning;', {
+    implicit: {meaning: '', vowels: [], '1.': {meaning: 'meaning', vowels: []}, '10.': {meaning: 'meaning', vowels: []}}
+  }],
+  [' **G** meaning **1.** meaning **2.** meaning;', {
+    'G': {meaning: 'meaning', vowels: [], '1.': {meaning: 'meaning', vowels: []}, '2.': {meaning: 'meaning', vowels: []}}
+  }],
+  [' **D** **1.** meaning;', {
+    'D': {meaning: '', vowels: [], '1.': {meaning: 'meaning', vowels: []}}
+  }],
+  [' **A.** meaning **B.** meaning;', {
+    'A.': {meaning: 'meaning', vowels: []},
+    'B.': {meaning: 'meaning', vowels: []}
+  }],
+  [' **A.** "meaning1; meaning2";', {
+    'A.': {meaning: '"meaning1; meaning2"', vowels: []}
+  }],
+  [' **A.** **1.** "meaning1; meaning2";', {
+    'A.': {meaning: '', vowels: [], '1.': {meaning: '"meaning1; meaning2"', vowels: []}}
+  }],
+  [' **1.** MB, NB (meaning) **2.** meaning', {
+    implicit: {meaning: '', vowels: [], '1.': {meaning: 'MB, NB (meaning)', vowels: []}, '2.': {meaning: 'meaning', vowels: []}}
+  }],
+  [' **G** meaning (*a/u*)', {
+    'G': {meaning: 'meaning (*a/u*)', vowels: extractVowels('meaning (*a/u*)')}
+  }],
+  [' **G** **1.** (*a/u*) "meaning";', {
+    'G': {meaning: '', vowels: [], '1.': {meaning: '(*a/u*) "meaning"', vowels: extractVowels('(*a/u*)')}}
+  }]
 ]
 
 describe('parseAmplifiedMeanings', () => {
